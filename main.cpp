220977f31339ddb326a8f0639e8f65c7157a672f
Andrew Rodionov <camelot58@gmail.com>
2018-03-12 14:49:56 -0700

    - удалена сложная конструкция IpFilter; - for_each, transform заменены на ranged-based for циклы; - исправлены и расширены тесты.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@
 #include <algorithm>
 #include <iterator>
 #include <stdint.h>
-#include "ip_filter.h"
+
+using IP = std::vector<uint8_t>;
 
 std::vector<std::string> split(const std::string &str, char d)
 {
@@ -27,10 +28,7 @@ std::vector<std::string> split(const std::string &str, char d)
   return r;
 }
 
-template <template <typename, typename> class Container, 
-          typename Value,
-          typename Allocator=std::allocator<Value> >
-std::ostream& operator<<(std::ostream& out, const Container<Value, Allocator>& ip)
+std::ostream& operator<<(std::ostream& out, const IP& ip)
 {
   for (auto ip_part = std::cbegin(ip); ip_part != std::cend(ip); ++ip_part)
   {
@@ -47,84 +45,42 @@ int main(int argc, char const *argv[])
 {
   try
   {
-    std::vector<std::vector<uint8_t> > ip_pool;
+    std::vector<IP> ip_pool;
 
     // Функцию split  по указанию оставил без имзменений.
     // А вот хранимый тип данных изменил.
     for (std::string line; std::getline(std::cin, line);)
     {
+      IP ip;
+      ip.reserve(4);
+
       std::vector<std::string> v = split(line, '\t');
-      v = split(v.at(0), '.');
-      std::vector<uint8_t> ip_parts;
-      ip_parts.reserve(4);
-      std::transform(std::begin(v), std::end(v), std::back_inserter(ip_parts), [](const auto& str) { return std::stoul(str); });
-      ip_pool.emplace_back(std::move(ip_parts));
+      v = split(v.at(0), '.');     
+
+      for(const auto& ip_string : v)
+        ip.push_back(std::stoul(ip_string));
+      ip_pool.emplace_back(std::move(ip));
     }
 
     std::sort(std::begin(ip_pool), std::end(ip_pool), std::greater<decltype(*std::begin(ip_pool))>());
-    std::for_each(std::cbegin(ip_pool), std::cend(ip_pool), [](const auto& ip) {std::cout << ip;} );
+    for(const auto& ip : ip_pool)
+      std::cout << ip;
 
-    // Самый простой способ, но нет повторяемости кроме как Ctrl+C, Ctrl+V.
-    // Из-за этого возникают вопросы с тестированием данного решения.
-    /*
-    std::for_each(
-      std::cbegin(ip_pool),
-      std::cend(ip_pool),
-      [](const auto& ip) {
-      if (1 == ip.at(0))
-        std::cout << ip; });
-
-    std::for_each(
-      std::cbegin(ip_pool),
-      std::cend(ip_pool),
-      [](const auto& ip) {
-      if ((46 == ip.at(0)) && (70 == ip.at(1)))
-        std::cout << ip; });
-
-    std::for_each(
-      std::cbegin(ip_pool),
-      std::cend(ip_pool),
-      [](const auto& ip) {
-      if (std::any_of(std::cbegin(ip), std::cend(ip), [](auto ip_part) { return 46 == ip_part; }))
-        std::cout << ip; });
-    */
-
-    // Более сложный способ с применением экземрляров класса IpFilter.
-    auto ip_filter_1 = make_ip_filter(make_ip_part_comparator(std::equal_to<uint8_t>(), 1));
     for(const auto& ip : ip_pool)
     {
-      if(ip_filter_1(ip))
+      if (1 == ip.at(0))
         std::cout << ip;
     }
 
-    reinit_ip_filter(
-      ip_filter_1,
-      make_ip_part_comparator(std::equal_to<uint8_t>(), 46),
-      make_ip_part_comparator(std::equal_to<uint8_t>(), 70));
-    for (const auto& ip : ip_pool)
+    for(const auto& ip : ip_pool)
     {
-      if (ip_filter_1(ip))
+      if ((46 == ip.at(0)) && (70 == ip.at(1)))
         std::cout << ip;
     }
 
-    reinit_ip_filter(
-      ip_filter_1,
-      make_ip_part_comparator(std::equal_to<uint8_t>(), 46));
-    auto ip_filter_2 = make_ip_filter(
-      make_ip_part_comparator(any_value<uint8_t>(), 0),
-      make_ip_part_comparator(std::equal_to<uint8_t>(), 46));
-    auto ip_filter_3 = make_ip_filter(
-      make_ip_part_comparator(any_value<uint8_t>(), 0),
-      make_ip_part_comparator(any_value<uint8_t>(), 0),
-      make_ip_part_comparator(std::equal_to<uint8_t>(), 46));
-    auto ip_filter_4 = make_ip_filter(
-      make_ip_part_comparator(any_value<uint8_t>(), 0),
-      make_ip_part_comparator(any_value<uint8_t>(), 0),
-      make_ip_part_comparator(any_value<uint8_t>(), 0),
-      make_ip_part_comparator(std::equal_to<uint8_t>(), 46));
-    for (const auto& ip : ip_pool)
+    for(const auto& ip : ip_pool)
     {
-      if (ip_filter_1(ip) || ip_filter_2(ip) || ip_filter_3(ip) || ip_filter_4(ip))
+      if (std::any_of(std::cbegin(ip), std::cend(ip), [](const auto& ip_part) { return 46 == ip_part; }))
         std::cout << ip;
     }
   }
--- test_main.cpp
+++ test_main.cpp
@@ -1,8 +1,8 @@
 #include "version.h"
-#include "ip_filter.h"
 
 #include <vector>
 #include <algorithm>
+#include <numeric>
 
 #define BOOST_TEST_MODULE test_main
 
@@ -15,46 +15,34 @@ BOOST_AUTO_TEST_CASE(test_version_valid)
   BOOST_CHECK( version() > version_info() );
 }
 
+using IP = std::vector<uint8_t>;
+
 BOOST_AUTO_TEST_CASE(test_ip_filter_valid)
 {
-  std::vector<std::vector<int>> ip_pool{{1,46,84,93}, {46,94,114,245}, {97,51,213,87}};
+  std::vector<IP> ip_pool{{1,46,84,93}, {46,94,114,245}, {97,51,213,87}};
 
-  auto any_ip_filter = make_ip_filter<int>();
-  BOOST_CHECK( ip_pool.size() == std::count_if(std::cbegin(ip_pool), std::cend(ip_pool), any_ip_filter) );
-
-  auto first_byte_46_filter = make_ip_filter(make_ip_part_comparator(std::equal_to<int>(), 46));
   for(const auto& ip : ip_pool)
   {
-    if(first_byte_46_filter(ip))
+    if(46 == ip.at(0))
       BOOST_CHECK( ip == ip_pool.at(1) );
   }
 
-  reinit_ip_filter(
-    any_ip_filter,
-    make_ip_part_comparator(any_value<int>(), 0),
-    make_ip_part_comparator(any_value<int>(), 0),
-    make_ip_part_comparator(std::equal_to<int>(), 213));
   for(const auto& ip : ip_pool)
   {
-    if(any_ip_filter(ip))
+    if(213 == ip.at(2))
       BOOST_CHECK( ip == ip_pool.at(2) );
   }
 
-  auto second_byte_46_filter = make_ip_filter(
-    make_ip_part_comparator(any_value<int>(), 0),
-    make_ip_part_comparator(std::equal_to<int>(), 46));
-  auto third_byte_46_filter = make_ip_filter(
-    make_ip_part_comparator(any_value<int>(), 0),
-    make_ip_part_comparator(any_value<int>(), 0),
-    make_ip_part_comparator(std::equal_to<int>(), 46));
-  auto fourth_byte_46_filter = make_ip_filter(
-    make_ip_part_comparator(any_value<int>(), 0),
-    make_ip_part_comparator(any_value<int>(), 0),
-    make_ip_part_comparator(any_value<int>(), 0),
-    make_ip_part_comparator(std::equal_to<int>(), 46));
-  BOOST_CHECK( 2 == std::count_if(std::cbegin(ip_pool), std::cend(ip_pool), [&](const auto& ip) {
-    return (first_byte_46_filter(ip) || second_byte_46_filter(ip) || third_byte_46_filter(ip) || fourth_byte_46_filter(ip));
-  }) );
+  BOOST_CHECK( 2 == std::count_if(std::cbegin(ip_pool),
+                                  std::cend(ip_pool),
+                                  [](const auto& ip)
+                                  {
+                                    return std::any_of(std::cbegin(ip),
+                                                      std::cend(ip),
+                                                      [](const auto& ip_part) { return 46 == ip_part; } );
+                                  }));
+
+  BOOST_CHECK( 224 == std::accumulate(std::cbegin(ip_pool.at(0)), std::cend(ip_pool.at(0)), 0) );
   
 }
 
